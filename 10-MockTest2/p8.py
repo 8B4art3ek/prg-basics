5647574981ed52c031fc8e5ccb2b8924df2cd4f6
Bartłomiej Wysokiński <bartek.wysokinski.1ib@gmail.com>
2025-12-14 14:55:22 +0100

    mala poprawka

--- 10-MockTest2/p8.py
+++ 10-MockTest2/p8.py
@@ -1,4 +1,4 @@
 # (p8.py) Odwrotna Notacja Polska (ONP) to notacja matematyczna, w której operatory następują po swoich operandach, np. 2 3 + 4 *. Zdefiniuj funkcję f(expression), która dla wyrażenia ONP zwraca wartość tego wyrażenia. Wyrażenie zawiera tylko nieujemne liczby całkowite oraz operatory + i -. 
 # Przykład: f("2 3 +") -> 5 
-# f("2 \ 6 + 4 \ 5 - +") -> 7
-# f("11 \ 7 + 15 - 14 +") -> 17
+# f("2 6 + 4 5 - +") -> 7
+# f("11 7 + 15 - 14 +") -> 17
